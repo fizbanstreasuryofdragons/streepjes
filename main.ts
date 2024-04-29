b3c225939fedd954875155a65c14592980eb8701
fizbanstreasuryofdragons <167873534+fizbanstreasuryofdragons@users.noreply.github.com>
2024-04-29 15:42:09 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,19 +1,47 @@
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         basic.showLeds(`
-            # . . . .
-            # . . . .
-            # . . . .
-            # . . . .
-            # . . . .
+            # # # # #
+            . . . . .
+            # # # # #
+            . . . . .
+            # # # # #
             `)
-    } else {
+        basic.pause(500)
         basic.showLeds(`
             . . . . .
+            # # # # #
             . . . . .
-            . . . . .
-            . . . . .
+            # # # # #
             . . . . .
             `)
+        basic.pause(500)
+    } else {
+        if (input.buttonIsPressed(Button.B)) {
+            basic.showLeds(`
+                # . # . #
+                # . # . #
+                # . # . #
+                # . # . #
+                # . # . #
+                `)
+            basic.pause(500)
+            basic.showLeds(`
+                . # . # .
+                . # . # .
+                . # . # .
+                . # . # .
+                . # . # .
+                `)
+            basic.pause(500)
+        } else {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
     }
 })
